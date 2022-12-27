6b84dddeab7967117a2aa1816dad205b20ff731d
cuicheng01 <cuicheng_smile@163.com>
2022-12-27 09:44:48 +0000

    update readme and use pre-commit

--- benchmark.py
+++ benchmark.py
@@ -9,6 +9,7 @@ import six
 
 #from resnet import ResNet50 as resnet50
 
+
 def str2bool(v):
     return v.lower() in ("true", "t", "1")
 
@@ -20,7 +21,7 @@ def init_args():
     parser.add_argument("--dy2static", type=str2bool, default=False)
     parser.add_argument("--use_amp", type=str2bool, default=False)
     parser.add_argument("--warmup_steps", type=int, default=300)
-    parser.add_argument("--run_steps", type=int, default=1000) 
+    parser.add_argument("--run_steps", type=int, default=1000)
     return parser
 
 
@@ -30,14 +31,19 @@ def parse_args():
 
 
 def print_args(args):
-    print ("-------------  Configuration Arguments -------------")
+    print("-------------  Configuration Arguments -------------")
     for arg, value in sorted(six.iteritems(vars(args))):
         print("%25s : %s" % (arg, value))
-    print ("----------------------------------------------------")
+    print("----------------------------------------------------")
 
 
 class MyModel(object):
-    def __init__(self, batch_size=32, use_amp=False, dy2static=False, warmup_steps=30, run_steps=100):
+    def __init__(self,
+                 batch_size=32,
+                 use_amp=False,
+                 dy2static=False,
+                 warmup_steps=30,
+                 run_steps=100):
 
         self.model = resnet50()
         if dy2static:
@@ -45,33 +51,47 @@ class MyModel(object):
         self.batch_size = batch_size
         self.use_amp = use_amp
         self.optimizer = paddle.optimizer.Adam(
-            parameters=self.model.parameters(),
-        )
+            parameters=self.model.parameters(), )
         self.loss_fn = paddle.nn.CrossEntropyLoss(soft_label=True)
         self.real_input = [paddle.randn((self.batch_size, 3, 224, 224))]
-        self.real_output = [paddle.nn.functional.one_hot(paddle.to_tensor([1]*self.batch_size, dtype='int64'), num_classes=1000)]
+        self.real_output = [
+            paddle.nn.functional.one_hot(
+                paddle.to_tensor(
+                    [1] * self.batch_size, dtype='int64'),
+                num_classes=1000)
+        ]
 
     def train(self):
         self.optimizer.clear_grad()
         for data, target in zip(self.real_input, self.real_output):
-           if self.use_amp == True:
-               scaler = paddle.amp.GradScaler(init_loss_scaling=1024)
-               with paddle.amp.auto_cast(custom_black_list={"flatten_contiguous_range", "greater_than"}, level="O1"):
-                   pred = self.model(data)
-                   loss = self.loss_fn(pred, target)
-               scaled = scaler.scale(loss).backward()
-               scaler.step(self.optimizer)
-               scaler.update()
-           else:
-               pred = self.model(data)
-               self.loss_fn(pred, target).backward()
-               self.optimizer.step()
+            if self.use_amp == True:
+                scaler = paddle.amp.GradScaler(init_loss_scaling=1024)
+                with paddle.amp.auto_cast(
+                        custom_black_list={
+                            "flatten_contiguous_range", "greater_than"
+                        },
+                        level="O1"):
+                    pred = self.model(data)
+                    loss = self.loss_fn(pred, target)
+                scaled = scaler.scale(loss).backward()
+                scaler.step(self.optimizer)
+                scaler.update()
+            else:
+                pred = self.model(data)
+                self.loss_fn(pred, target).backward()
+                self.optimizer.step()
+
 
 if __name__ == "__main__":
     args = parse_args()
     print_args(args)
-    model = MyModel(batch_size=args.batch_size, use_amp=args.use_amp, dy2static=args.dy2static)
+
+    model = MyModel(
+        batch_size=args.batch_size,
+        use_amp=args.use_amp,
+        dy2static=args.dy2static)
     place = paddle.CUDAPlace(0)
+
     latency_list = []
     for i in range(args.run_steps):
         if i < args.warmup_steps:
@@ -80,5 +100,5 @@ if __name__ == "__main__":
             t1 = time.time()
             model.train()
             t2 = time.time()
-            latency_list.append(t2-t1)
-    print ("IPS: {} img/s".format(args.batch_size/np.mean(latency_list)))
+            latency_list.append(t2 - t1)
+    print("IPS: {} img/s".format(args.batch_size / np.mean(latency_list)))
